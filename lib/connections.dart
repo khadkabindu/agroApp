b097637151c3786b88c4f7dc3463d33d8ba4abff
Bindu Khadka <bindu@Rajeshs-MacBook-Pro.local>
2020-06-05 11:39:51 +0545

    added connections

--- lib/connections.dart
+++ lib/connections.dart
@@ -1,20 +1,22 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Connections extends StatefulWidget{
+class Connections extends StatefulWidget {
   @override
   _Connections createState() => _Connections();
 }
 
-class _Connections extends State<Connections>{
+class _Connections extends State<Connections> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text('Connections',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),),
+          title: Text(
+            'Connections',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
           elevation: 0.0,
           backgroundColor: Colors.white,
           leading: IconButton(
@@ -27,35 +29,50 @@ class _Connections extends State<Connections>{
             IconButton(
                 color: Colors.black,
                 icon: Icon(Icons.search),
-                onPressed: () {
-
-                }),
+                onPressed: () {}),
           ],
         ),
         body: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-
-              Row(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundImage:
-                    AssetImage('images/woman farmer.jpg'),
+          child: ListView.separated(
+              scrollDirection: Axis.vertical,
+              itemCount: 10,
+              separatorBuilder: (context, index) => Divider(
+                    height: 1,
+                    color: Colors.grey,
                   ),
-                  Text(
-                    'Reema Basnet',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15.0),
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8,),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundImage:
+                                AssetImage('images/woman farmer.jpg'),
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Text(
+                                'Reema Basnet                             ',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15.0),
+                              ),
+                              Text(
+                                '  Saman kaile deliver hunxa Reema ?',
+                                style: TextStyle(color: Colors.blueGrey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
-
-
-            ],
-          ),
+                );
+              }),
         ),
       ),
     );
